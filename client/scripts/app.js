40f66e7e169897b79f2707fa3c9022fa590d3f91
Hack Reactor Student <student@hackreactor.com>
2017-07-14 17:25:19 -0700

    Complete through clearMessages
    Debugging renderMessage

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,85 @@
 // YOUR CODE HERE:
+var app = {};
+
+app.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
+
+app.init = function() {};
+
+app.send = function(message) {
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+    url: this.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: (data) => {
+      console.log('chatterbox: Message sent');
+    },
+    error: (data) => {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = function() {
+
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+    url: this.server,
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: (data) => {
+      console.log('chatterbox: Message received');
+    },
+    error: (data) => {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to receive message', data);
+    }
+  });
+};
+
+app.clearMessages = () => $('#chats').html(' ');
+
+
+app.renderMessage = (message) => $('#chats').prepend(`${message.username}: ${message.text}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
